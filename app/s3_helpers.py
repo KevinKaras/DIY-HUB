71502bf4f5287979386eedbd081bfd706fc3f418
KevinKaras <kevinkaras2@gmail.com>
2021-07-21 22:28:21 -0500

    might actually need this

--- app/s3_helpers.py
+++ app/s3_helpers.py
@@ -1,20 +1,20 @@
 # # nothing on this page was implemented or used I believe
 # will remain commented out until purpose arises
 
-# import boto3
-# import botocore
-# import os
-# import uuid
+import boto3
+import botocore
+import os
+import uuid
 
-# BUCKET_NAME = os.environ.get("S3_BUCKET")
-# S3_LOCATION = f"http://{BUCKET_NAME}.s3.amazonaws.com/"
-# ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
+BUCKET_NAME = os.environ.get("S3_BUCKET")
+S3_LOCATION = f"http://{BUCKET_NAME}.s3.amazonaws.com/"
+ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
 
-# s3 = boto3.client(
-#    "s3",
-#    aws_access_key_id=os.environ.get("S3_KEY"),
-#    aws_secret_access_key=os.environ.get("S3_SECRET")
-# )
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=os.environ.get("S3_KEY"),
+   aws_secret_access_key=os.environ.get("S3_SECRET")
+)
 
 
 # def allowed_file(filename):
