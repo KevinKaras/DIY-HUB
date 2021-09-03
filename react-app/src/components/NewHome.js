5a44f64df4dffafe829a6557c016fc4ff24cd42f
KevinKaras <kevinkaras2@gmail.com>
2021-09-03 13:46:13 -0500

    changed dots, base structure almost done, heading to lunch

--- react-app/src/components/NewHome.js
+++ react-app/src/components/NewHome.js
@@ -1,24 +1,39 @@
-<div classname="BannerContainer">
-    <div classname="BannerPhotoDiv">
-    
-    </div>
-    <div classname="BannerInformationContainer">
-        <div classname="InformationCard">
-            <div classname="InfoCardTitle">
+<div>
+    <div classname="Banner-Container">
+        <div classname="Banner-Photo-Div">
+        
+        </div>
+        <div classname="Banner-Information-Container">
+            <div classname="Information-Card">
+                <div classname="InfoCard-Title">
 
+                </div>
+                <div classname="Info-Card-Description">
+
+                </div>
             </div>
-            <div classname="InfoCardDescription">
-
+            <div classname="Dot-Scroll-Wheel">
+                <li classname="Scroll-Dots"></li>
+                <li classname="Scroll-Dots"></li>
+                <li classname="Scroll-Dots"></li>
+                <li classname="Scroll-Dots"></li>
+                <li classname="Scroll-Dots"></li>
             </div>
         </div>
-        <div classname="DotScrollWheel">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
     </div>
+    <div classname="Home-Content-Container">
+        <div classname="Content-Container">
+            <div classname="Container-Topic">
+            </div>
+            <div classname="Container-Paragraph">
+            </div>
+        </div>
+
+    </div>
+
+
+
 </div>
+    
 
 
