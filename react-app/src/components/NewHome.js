e5c51a2f5baea28c3255e4a4cf0b5e0222287537
KevinKaras <kevinkaras2@gmail.com>
2021-09-03 09:19:05 -0500

    banner photo layout complete

--- react-app/src/components/NewHome.js
+++ react-app/src/components/NewHome.js
@@ -1,3 +1,24 @@
-// testing if commits are working or not 
+<div classname="BannerContainer">
+    <div classname="BannerPhotoDiv">
+    
+    </div>
+    <div classname="BannerInformationContainer">
+        <div classname="InformationCard">
+            <div classname="InfoCardTitle">
+
+            </div>
+            <div classname="InfoCardDescription">
+
+            </div>
+        </div>
+        <div classname="DotScrollWheel">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</div>
 
 
