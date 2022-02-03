8646adafb3a794f2c32db29db13b7d7c1f37a30e
KevinKaras <kevinkaras2@gmail.com>
2022-02-03 09:04:52 -0500

    removed postpage wireframe layout

--- app/models/comment.py
+++ app/models/comment.py
@@ -18,6 +18,3 @@ class Comment(db.Model):
             "postid": self.postid,
             "commentText": self.commentText
         }
-
-
-
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -51,5 +51,4 @@ function App() {
     
   );
 }
-
 export default App;
